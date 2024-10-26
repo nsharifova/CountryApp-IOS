348ee3a9e400112a812ba35d0cb8b9ea1a76b403
nsharifova <nurluay00@bk.ru>
2024-10-26 14:12:11 +0400

    new commit

--- MyCountryApp/SceneDelegate.swift
+++ MyCountryApp/SceneDelegate.swift
@@ -13,16 +13,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = scene as? UIWindowScene else { return }
-           
-           let window = UIWindow(windowScene: windowScene)
-           let storyboard = UIStoryboard(name: "Main", bundle: nil)
-           let initialViewController = storyboard.instantiateViewController(withIdentifier: "countryCV") as! CountyViewController
-           
-           let navigationController = UINavigationController(rootViewController: initialViewController) // UINavigationController ekleyin
-           window.rootViewController = navigationController
-           self.window = window
-           window.makeKeyAndVisible()
+//        guard let windowScene = scene as? UIWindowScene else { return }
+//           
+//           let window = UIWindow(windowScene: windowScene)
+//           let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//           let initialViewController = storyboard.instantiateViewController(withIdentifier: "countryCV") as! CountyViewController
+//           
+//           let navigationController = UINavigationController(rootViewController: initialViewController) // UINavigationController ekleyin
+//           window.rootViewController = navigationController
+//           self.window = window
+//           window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
